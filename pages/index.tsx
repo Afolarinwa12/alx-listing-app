624743ca18659c1eb29ffbb562d31bc0951da8df
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-05-15 23:48:30 +0100

    new nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App! (Pages Router)</h1>
+      <p>This is the home page rendered by the Pages Router.</p>
+    </div>
+  );
+};
+
+export default Home;
